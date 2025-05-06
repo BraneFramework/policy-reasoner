94924462d74a4a34a7913cd8285cb5f8ba0eb653
Lut99 <mullertim99@gmail.com>
2025-05-06 11:17:41 +0200

    Added `eflint-haskell` example

---
+++ examples/eflint_haskell.rs
@@ -0,0 +1,129 @@
+//  EFLINT HASKELL.rs
+//    by Lut99
+//
+//  Created:
+//    06 May 2025, 11:09:11
+//  Last edited:
+//    06 May 2025, 11:17:02
+//  Auto updated?
+//    Yes
+//
+//  Description:
+//!   Showcases using the `eflint-haskell-reasoner` backend of the policy
+//!   reasoner.
+//
+
+use std::path::PathBuf;
+
+use clap::Parser;
+use console::style;
+use error_trace::trace;
+use policy_reasoner::loggers::file::FileLogger;
+use policy_reasoner::reasoners::eflint_haskell::EFlintHaskellReasonerConnector;
+use policy_reasoner::reasoners::eflint_haskell::reasons::SilentHandler;
+use policy_reasoner::spec::auditlogger::SessionedAuditLogger;
+use policy_reasoner::spec::reasonerconn::ReasonerConnector as _;
+use policy_reasoner::spec::reasons::NoReason;
+use spec::reasonerconn::ReasonerResponse;
+use tracing::{Level, error, info};
+
+
+/***** ARGUMENTS *****/
+/// Defines the arguments for this binary.
+#[derive(Parser)]
+struct Arguments {
+    /// Whether to make `info!()` and `debug!()` visible.
+    #[clap(long, help = "If given, enables INFO- and DEBUG-level logging.")]
+    debug: bool,
+    /// Whether to make `trace!()` visible.
+    #[clap(long, help = "If given, enables TRACE-level logging. Implies '--debug'.")]
+    trace: bool,
+
+    /// The file to use as input.
+    #[clap(name = "FILE", default_value = "-", help = "The eFLINT (JSON) file to read. Use '-' to read from stdin.")]
+    file: String,
+
+    /// Which `eflint-repl` to use.
+    #[clap(short, long, default_value = "eflint-repl", help = "The command used to launch the `eflint-repl` binary.")]
+    eflint_cmd: String,
+}
+
+
+
+
+
+/***** LIBRARY *****/
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // Parse the arguments
+    let args = Arguments::parse();
+
+    // Setup the logger
+    tracing_subscriber::fmt()
+        .with_max_level(if args.trace {
+            Level::TRACE
+        } else if args.debug {
+            Level::DEBUG
+        } else {
+            Level::WARN
+        })
+        .init();
+    info!("{} - v{}", env!("CARGO_BIN_NAME"), env!("CARGO_PKG_VERSION"));
+
+    // Create the logger
+    let mut logger: SessionedAuditLogger<FileLogger> =
+        SessionedAuditLogger::new("test", FileLogger::new(format!("{} - v{}", env!("CARGO_BIN_NAME"), env!("CARGO_PKG_VERSION")), "./test.log"));
+
+    // Ensure there is a file to input
+    let policy: PathBuf = if args.file == "-" {
+        let file: PathBuf = std::env::temp_dir().join(format!("{}-v{}-stdin.eflint", env!("CARGO_BIN_NAME"), env!("CARGO_PKG_VERSION")));
+        let mut handle: tokio::fs::File = match tokio::fs::File::create(&file).await {
+            Ok(handle) => handle,
+            Err(err) => {
+                error!("{}", trace!(("Failed to open temporary stdin file '{}'", file.display()), err));
+                std::process::exit(1);
+            },
+        };
+        if let Err(err) = tokio::io::copy(&mut tokio::io::stdin(), &mut handle).await {
+            error!("{}", trace!(("Failed to write stdin to temporary file '{}'", file.display()), err));
+            std::process::exit(1);
+        }
+        file
+    } else {
+        PathBuf::from(args.file)
+    };
+
+    // Create the reasoner
+    let conn = match EFlintHaskellReasonerConnector::<SilentHandler, String, ()>::new_async(
+        shlex::split(&args.eflint_cmd).into_iter().flatten(),
+        &policy,
+        SilentHandler,
+        &mut logger,
+    )
+    .await
+    {
+        Ok(conn) => conn,
+        Err(err) => {
+            error!("{}", trace!(("Failed to create eFLINT reasoner"), err));
+            std::process::exit(1);
+        },
+    };
+    let verdict: ReasonerResponse<NoReason> = match conn.consult("".into(), (), &mut logger).await {
+        Ok(res) => res,
+        Err(err) => {
+            error!("{}", trace!(("Failed to send message to reasoner {:?}", args.eflint_cmd), err));
+            std::process::exit(1);
+        },
+    };
+
+    // OK, report
+    match verdict {
+        ReasonerResponse::Success => println!("{} {}", style("Reasoner says:").bold(), style("OK").bold().green()),
+        ReasonerResponse::Violated(reasons) => {
+            println!("{} {}", style("Reasoner says:").bold(), style("VIOLATION").bold().red());
+            println!("Reason:");
+            println!("{reasons}");
+            println!();
+        },
+    }
+}
